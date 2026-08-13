e86309c553635579241b4d4455422ede29e9ca83
yokkoshima <skurek@forus.ru>
2026-08-13 17:23:23 +0800

    chore: docker-compose with postgres, envs

--- src/env.ts
+++ src/env.ts
@@ -2,27 +2,34 @@ import "dotenv/config";
 import z from "zod";
 import { fromError } from "zod-validation-error";
 
-const envSchema = z.object({
-  RPC_URL: z.url(),
-});
+const envSchema = z
+  .object({
+    RPC_URL: z.url(),
+    POSTGRES_USER: z.string().min(1),
+    POSTGRES_PASSWORD: z.string().min(1),
+    POSTGRES_DB: z.string().min(1),
+    POSTGRES_HOST: z.string().default("localhost"),
+    POSTGRES_PORT: z.coerce.number().default(5433),
+  })
+  .transform((e) => ({
+    ...e,
+    DATABASE_URL: `postgres://${e.POSTGRES_USER}:${e.POSTGRES_PASSWORD}@${e.POSTGRES_HOST}:${e.POSTGRES_PORT}/${e.POSTGRES_DB}`,
+  }));
 
 export type Env = z.infer<typeof envSchema>;
-let env: Env;
 
-try {
-  env = envSchema.parse(process.env);
-} catch (e) {
-  if (e instanceof z.ZodError) {
-    const validationError = fromError(e, {
-      prefix: "dotenv config validation error",
-    });
+const parsed = envSchema.safeParse(process.env);
 
-    console.error("❌ " + validationError.message);
-    process.exit(1);
-  }
-
-  throw e;
+if (!parsed.success) {
+  console.error(
+    "❌ ",
+    fromError(parsed.error, {
+      prefix: "env validation error",
+    }),
+  );
 }
+
+const env = parsed.data!;
 
 export { env };
 export default env;
